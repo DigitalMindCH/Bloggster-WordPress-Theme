049358b9eed64664f71fed0fe4604ff6051aec91
Myriam <myriam@duckstance.com>
2015-12-01 15:12:22 +0100

    renaming theme

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
-# Blogster WordPress Theme
+# Bloggster WordPress Theme
 
-This repo features documentation and other helpful files for the work with Blogster.
+This repo features documentation and other helpful files for the work with Bloggster.
 
-For documentation please refer to the [Wiki](https://github.com/DigitalMindCH/Blogster-WordPress-Theme/wiki).
+For documentation please refer to the [Wiki](https://github.com/DigitalMindCH/Bloggster-WordPress-Theme/wiki).
--- blogster-child/functions.php
+++ blogster-child/functions.php
@@ -3,6 +3,6 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css' );
 }
 
-// priority of 99 ensures that the style.css is added after the blogster css, thus enables the child theme to overwrite the styles of the theme. 
+// priority of 99 ensures that the style.css is added after the bloggster css, thus enables the child theme to overwrite the styles of the theme. 
 add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles', '99' );
 ?>
